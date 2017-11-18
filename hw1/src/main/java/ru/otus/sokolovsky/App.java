0bb734bc5ac336e82f96bf0937b8958c778ee984
Максим Соколовский <sokolovsky@worksolutions.ru>
2017-11-18 17:32:54 +0300

    HW1. Creating project stub with apache common cli dependency.

---
+++ hw1/src/main/java/ru/otus/sokolovsky/App.java
@@ -0,0 +1,15 @@
+
+package ru.otus.sokolovsky;
+
+/**
+ * The demonstrating class that doing nothing special
+ */
+public class App {
+
+    /**
+     * runs the process
+     */
+    static public void main(String[] args) {
+        System.out.println("ok");
+    }
+}
