a95d7f0a2e1656d47173e6af8a80aea0c9326680
Максим Соколовский <sokolovsky@worksolutions.ru>
2017-11-18 20:20:22 +0300

    HW1. Finished the programm which prints a version number of build

---
+++ hw1/src/main/java/ru/otus/sokolovsky/hw1/App.java
@@ -0,0 +1,61 @@
+
+package ru.otus.sokolovsky.hw1;
+
+import org.apache.commons.cli.*;
+
+/**
+ * The cli application that is doing nothing but print the version pointed in pom.xml.
+ *
+ * usage:
+ *  mvn clean
+ *  mvn package
+ *
+ *  java -jar target/ru.otus.sokolovsky-hw1\(1.0-SNAPSHOT\)-jar-with-dependencies.jar
+ *  java -jar java -jar target/ru.otus.sokolovsky-hw1\(1.0-SNAPSHOT\)-jar-with-dependencies.jar -version
+ *
+ */
+public class App {
+
+    /**
+     * runs the process
+     */
+    static public void main(String[] args) {
+        DefaultParser parser = new DefaultParser();
+        CommandLine cli;
+        try {
+            cli = parser.parse(createOptions(), args);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return;
+        }
+        if (cli.hasOption("version")) {
+            System.out.format("%s version \"%s\"\n", getProjectName(), getProjectVersion());
+        } else {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp(getProjectName(), createOptions());
+        }
+    }
+
+    /**
+     * Creates demonstrated options
+     */
+    private static Options createOptions() {
+        Options options = new Options();
+        options.addOption(buildVersionOption());
+        return options;
+    }
+
+    private static Option buildVersionOption() {
+        return Option.builder("version")
+                .desc("Prints version of the project")
+                .build();
+    }
+
+    private static String getProjectVersion() {
+        return App.class.getPackage().getImplementationVersion();
+    }
+
+    private static String getProjectName() {
+        return App.class.getPackage().getImplementationTitle();
+    }
+}
