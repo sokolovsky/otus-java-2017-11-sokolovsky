c84e03f5cf7ab395a9b7a68c6c0feadf452189ce
Максим Соколовский <sokolovsky@worksolutions.ru>
2017-12-02 19:59:49 +0300

    HW3. The skeleton of the future project was created

---
+++ hw03/src/main/java/ru/otus/sokolovsky/hw3/CircularBufferQueue.java
@@ -0,0 +1,79 @@
+package ru.otus.sokolovsky.hw3;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+public class CircularBufferQueue <E> implements Queue<E> {
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    public boolean add(E e) {
+        return false;
+    }
+
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    public void clear() {
+
+    }
+
+    public boolean offer(E e) {
+        return false;
+    }
+
+    public E remove() {
+        return null;
+    }
+
+    public E poll() {
+        return null;
+    }
+
+    public E element() {
+        return null;
+    }
+
+    public E peek() {
+        return null;
+    }
+}
---
+++ hw03/src/test/java/ru/otus/sokolovsky/hw3/CircularBufferQueueTest.java
@@ -0,0 +1,14 @@
+package ru.otus.sokolovsky.hw3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CircularBufferQueueTest {
+
+    @Test
+    public void warmup() {
+        CircularBufferQueue<Integer> q = new CircularBufferQueue<Integer>();
+    }
+}
