f58a9446029548a0b5b07099490010d1d6c74be3
Максим Соколовский <sokolovsky@worksolutions.ru>
2017-12-12 21:13:38 +0300

    HW4. Output measure into file

--- hw04/src/main/java/ru/otus/sokolovsky/hw4/App.java
+++ hw04/src/main/java/ru/otus/sokolovsky/hw4/App.java
@@ -1,5 +1,8 @@
 package ru.otus.sokolovsky.hw4;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 
@@ -19,13 +22,22 @@ import static java.lang.management.ManagementFactory.*;
  после начала работы.
  Собрать статистику (количество сборок, время на сборрки) по разным типам GC.
  Сделать выводы.
+
+ params
+    * logname - filename of log file for writing current directory
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
         System.out.println("CPU cores count: " + Runtime.getRuntime().availableProcessors());
 
-        GCRunsMeasurer GCReporter = new GCRunsMeasurer(getGarbageCollectorMXBeans(), System.out);
+        String logfile = "default.log";
+        if (args.length > 0 && args[0].length() > 0) {
+            logfile = args[0];
+        }
+        PrintStream stream = new PrintStream(new FileOutputStream("./" + logfile));
+
+        GCRunsMeasurer GCReporter = new GCRunsMeasurer(getGarbageCollectorMXBeans(), stream);
         Thread reporterThread = new Thread(GCReporter);
         reporterThread.start();
         long oomTotalMemory;
@@ -41,11 +53,12 @@ public class App {
             RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
             long uptimeOfSec = rb.getUptime() / 1000;
 
-            System.out.printf("\nMemory (%s) ran out, time %d sec\n", mem, uptimeOfSec);
+            System.out.printf("\nMemory (%s) was run out, time %d sec\n", mem, uptimeOfSec);
             GCReporter.publishCommonMeasure();
             throw e;
         } finally {
             reporterThread.interrupt();
+            stream.close();
         }
     }
 }
