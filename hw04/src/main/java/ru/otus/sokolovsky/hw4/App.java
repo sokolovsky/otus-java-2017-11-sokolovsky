820884912553f365b9c69d57070f71f7379240e5
Максим Соколовский <sokolovsky@worksolutions.ru>
2017-12-12 22:39:14 +0300

    HW4. Removing redundant comments in main class

--- hw04/src/main/java/ru/otus/sokolovsky/hw4/App.java
+++ hw04/src/main/java/ru/otus/sokolovsky/hw4/App.java
@@ -9,20 +9,7 @@ import java.lang.management.RuntimeMXBean;
 import static java.lang.management.ManagementFactory.*;
 
 /**
- GC
- • VM options
- • JMX + mbeans + jconsole
- • Thread and heap dumps
-
- Написать приложение, которое следит за сборками мусора и пишет в лог количество сборок каждого типа
- (young, old) и время которое ушло на сборки в минуту.
- Добиться OutOfMemory в этом приложении через медленное подтекание по памяти (например добавлять
- элементы в List и удалять только половину).
- Настроить приложение (можно добавлять Thread.sleep(...)) так чтобы оно падало с OOM примерно через 5 минут
- после начала работы.
- Собрать статистику (количество сборок, время на сборрки) по разным типам GC.
- Сделать выводы.
-
+ * 
  params
     * logname - filename of log file for writing current directory
  */
