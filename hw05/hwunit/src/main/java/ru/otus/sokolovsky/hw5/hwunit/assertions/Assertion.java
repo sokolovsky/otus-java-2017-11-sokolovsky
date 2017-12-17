fd95ba191c2e9711edcffadc338356eff125b8bf
Максим Соколовский <sokolovsky@worksolutions.ru>
2017-12-17 16:36:04 +0300

    HW5. Definitions of simple framework assertions

---
+++ hw05/hwunit/src/main/java/ru/otus/sokolovsky/hw5/hwunit/assertions/Assertion.java
@@ -0,0 +1,14 @@
+package ru.otus.sokolovsky.hw5.hwunit.assertions;
+
+public class Assertion {
+    public static void assertNotNull(Object actual) {
+
+    }
+
+    public static void assertEquals(Object actual, Object expected) {
+
+    }
+
+    public static void assertTrue(boolean actual) {
+    }
+}
---
+++ hw05/hwunit/src/main/java/ru/otus/sokolovsky/hw5/hwunit/assertions/AssertionException.java
@@ -0,0 +1,40 @@
+package ru.otus.sokolovsky.hw5.hwunit.assertions;
+
+public class AssertionException extends RuntimeException {
+
+    private String message;
+    private Object actual;
+    private Object expected;
+
+    AssertionException(String message, Object actual) {
+        this.message = message;
+        this.actual = actual;
+    }
+
+    AssertionException(String message, Object actual, Object expected) {
+        this.message = message;
+        this.actual = actual;
+        this.expected = expected;
+    }
+
+    public String getExpected() {
+        if (expected == null) {
+            return "";
+        }
+        return expected.toString();
+    }
+
+    public String getActual() {
+        return actual.toString();
+    }
+
+    @Override
+    public String getMessage() {
+        return String.format(
+                "\n%s\nActual:%s\nExpected:%s\n",
+                message,
+                getActual(),
+                getExpected()
+        );
+    }
+}
