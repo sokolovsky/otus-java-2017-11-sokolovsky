cc5f80e67506d94efff52150c5c14d58f9062189
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-01-07 20:28:18 +0300

    HW06. Terminal class for IO exchange

---
+++ hw06/src/main/java/ru/otus/sokolovsky/hw6/terminal/Terminal.java
@@ -0,0 +1,41 @@
+package ru.otus.sokolovsky.hw6.terminal;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Terminal {
+
+    private final Writer out;
+    private final Reader in;
+    private String prompt = "/>";
+
+    public Terminal(Writer out, Reader in) {
+        this.out = out;
+        this.in = in;
+    }
+
+    public String getLine() throws IOException {
+        out.write(prompt);
+        out.flush();
+        return readLine();
+    }
+
+    private String readLine() {
+        Scanner scanner = new Scanner(in);
+        return scanner.nextLine();
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+
+    public void writeln(String line) {
+        try {
+            out.write(line);
+            out.write("\n");
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
