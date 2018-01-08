7bf3770b090c5f2fa93a054904a49d2e97bc749c
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-01-08 18:22:56 +0300

    HW06. Final preparation of account methods

--- hw06/src/main/java/ru/otus/sokolovsky/hw6/accounting/Account.java
+++ hw06/src/main/java/ru/otus/sokolovsky/hw6/accounting/Account.java
@@ -1,16 +1,25 @@
 package ru.otus.sokolovsky.hw6.accounting;
 
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Account {
     private String number;
     private int amount;
+    private List<String> history = new LinkedList<>();
 
     Account(String number, int amount) {
         this.number = number;
         this.amount = amount;
     }
 
-    public void credit(int amount) {
-        this.amount += amount;
+    public List<String> history() {
+        return history;
+    }
+
+    public void writeToHistory(String operation, int amount) {
+        history.add(String.format("%tD / %s / %,d", LocalDateTime.now(), operation, amount));
     }
 
     public void withdraw(int amount) throws WrongOperationException {
@@ -18,6 +27,12 @@ public class Account {
             throw new WrongOperationException();
         }
         this.amount -= amount;
+        writeToHistory("withdraw", amount);
+    }
+
+    public void credit(int amount) {
+        this.amount += amount;
+        writeToHistory("credit", amount);
     }
 
     public int getBalance() {
