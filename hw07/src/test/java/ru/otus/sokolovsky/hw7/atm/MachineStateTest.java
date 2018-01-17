f25d3bf4b95f6b8e4b70df9a0408af70d9c654d8
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-01-17 21:04:32 +0300

    HW07. Bug is detected

--- hw07/src/test/java/ru/otus/sokolovsky/hw7/atm/MachineStateTest.java
+++ hw07/src/test/java/ru/otus/sokolovsky/hw7/atm/MachineStateTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class MachineStateTest {
 
     @Test
-    public void refreshFromState() {
+    public void useFromState() {
         Machine machine = new Machine();
 
         machine.putMoney(Note.ONE_HUNDRED, 2);
@@ -31,5 +31,21 @@ public class MachineStateTest {
         assertEquals(500, machine.getAmount());
         Machine.State state = machine.createState();
         assertEquals(2, state.getCells().size());
+    }
+
+    @Test
+    public void refreshState() {
+        Machine machine = new Machine();
+
+        machine.putMoney(Note.ONE_HUNDRED, 2);
+        machine.putMoney(Note.HALF_OF_HUNDRED, 6);
+
+        Machine.State state = machine.createState();
+
+        machine.putMoney(Note.ONE_HUNDRED, 10);
+        machine.setState(state);
+
+        assertEquals(500, machine.getAmount());
+        assertEquals(2, machine.getCellsInfo().size());
     }
 }
