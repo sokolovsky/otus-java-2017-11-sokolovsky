10f7a3ca26d63fb5474eda4f093e6e3763e006bf
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-01-21 20:20:13 +0300

    HW08. Init simple structure for doing homework.

---
+++ hw8/src/main/java/ru/otus/sokolovsky/hw8/JsonWriter.java
@@ -0,0 +1,4 @@
+package ru.otus.sokolovsky.hw8;
+
+public class JsonWriter {
+}
---
+++ hw8/src/test/java/ru/otus/sokolovsky/hw8/JsonWriterTest.java
@@ -0,0 +1,11 @@
+package ru.otus.sokolovsky.hw8;
+
+import static org.junit.Assert.*;
+
+public class JsonWriterTest {
+
+
+    public void createList() {
+
+    }
+}
