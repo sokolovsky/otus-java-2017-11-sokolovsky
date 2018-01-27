5b31e53d510a9f9a9e4229556eef44d4b930d410
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-01-27 12:56:51 +0300

    HW08. Creating a sample object for testing.

---
+++ hw8/src/test/java/ru/otus/sokolovsky/hw8/SampleObject.java
@@ -0,0 +1,19 @@
+package ru.otus.sokolovsky.hw8;
+
+import java.util.List;
+import java.util.Map;
+
+public class SampleObject {
+    public int iNumber;
+    public double dNumber;
+    public boolean bool;
+    public String str;
+    public SampleObject obj;
+
+    public int[] arNumber;
+    public String[] arString;
+
+    public Object[] array;
+    public List<Object> list;
+    public Map<String, ?> map;
+}
