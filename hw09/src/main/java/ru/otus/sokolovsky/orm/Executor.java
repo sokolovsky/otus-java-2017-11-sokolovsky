b015cc05032b4e12e840230dcc8e71f310fdde9f
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-01-31 17:37:53 +0300

    HW09. Use bind methods of prepared statement for increase of safety.

--- hw09/src/main/java/ru/otus/sokolovsky/orm/Executor.java
+++ hw09/src/main/java/ru/otus/sokolovsky/orm/Executor.java
@@ -1,14 +1,11 @@
 package ru.otus.sokolovsky.orm;
 
+import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.sql.*;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Executor {
 
@@ -18,17 +15,22 @@ public class Executor {
         this.connection = connection;
     }
 
-    public int execUpdate(String sql) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sql);
+    public int execUpdate(String sql, List<ValueContainer> values) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            setPreparedValues(statement, values);
+            statement.executeUpdate();
             return statement.getUpdateCount();
         }
     }
 
-    public long execInsert(String sql) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+    public int execUpdate(String sql) throws SQLException {
+        return execUpdate(sql, new ArrayList<>());
+    }
 
+    public long execInsert(String sql, List<ValueContainer> values) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            setPreparedValues(statement, values);
+            statement.executeUpdate();
             try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
                 if (generatedKeys.next()) {
                     return (generatedKeys.getLong(1));
@@ -39,9 +41,14 @@ public class Executor {
         }
     }
 
-    public void execSelect(String sql, RecordSetHandler handler) throws SQLException {
-        try (Statement statement = connection.createStatement()) {
-            statement.execute(sql);
+    public long execInsert(String sql) throws SQLException {
+        return execInsert(sql, new ArrayList<>());
+    }
+
+    public void execSelect(String sql, List<ValueContainer> values, RecordSetHandler handler) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            setPreparedValues(statement, values);
+            statement.executeQuery();
             ResultSet resultSet = statement.getResultSet();
             while (resultSet.next()) {
                 handler.handle(resultSet);
@@ -49,15 +56,22 @@ public class Executor {
         }
     }
 
+    public void execSelect(String sql, RecordSetHandler handler) throws SQLException {
+        execSelect(sql, new ArrayList<>(), handler);
+    }
+
     public <T extends DataSet> void save(T model) {
         EntityDefinition<T> definition = EntityDefinitionsRegister.getDefinition((Class<T>) model.getClass());
-        Map<String, String> pairs = definition.getFieldsPairs(model);
+        Map<String, ValueContainer> values = definition.getFieldsValues(model);
+        for (String primary : definition.getPrimaries()) {
+            values.remove(primary);
+        }
 
         if (model.getId() == 0) {
-            long id = insert(definition.tableName(), pairs);
+            long id = insert(definition.tableName(), values);
             model.setId(id);
         } else {
-            update(definition.tableName(), pairs, model.getId());
+            update(definition.tableName(), values, model.getId());
         }
     }
 
@@ -70,9 +84,12 @@ public class Executor {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
-        String sql = "SELECT * from %s where id=%d";
+        String sql = "SELECT * from %s where id = ? ";
         try {
-            execSelect(String.format(sql, definition.tableName(), id), (resultSet -> {
+            LinkedList<ValueContainer> values = new LinkedList<>();
+            values.add(new ValueContainer(ValueContainer.Type.LONG, id));
+
+            execSelect(String.format(sql, definition.tableName()), values,(resultSet -> {
                 try {
                     int columnCount = resultSet.getMetaData().getColumnCount();
                     Map<String, String> pairs = new HashMap<>();
@@ -92,33 +109,65 @@ public class Executor {
         return model;
     }
 
-    private void update(String table, Map<String, String> pairs, long id) {
-        String sql = "UPDATE %s SET %s WHERE id=%d";
+    private void update(String table, Map<String, ValueContainer> map, long id) {
+        String sql = "UPDATE `%s` SET %s where id = ?";
 
-        sql = String.format(sql, table, collectSqlSet(pairs), id);
+        List<String> columns = new ArrayList<>(map.keySet());
+        sql = String.format(sql, table, collectSqlSet(columns));
+        List<ValueContainer> values = columns.stream().map(map::get).collect(Collectors.toList());
+        values.add(new ValueContainer(ValueContainer.Type.LONG, id));
 
         try {
-            execUpdate(sql);
+            execUpdate(sql, values);
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
 
-    private long insert(String table, Map<String, String> pairs) {
+    private long insert(String table, Map<String, ValueContainer> map) {
         String sql = "INSERT INTO %s SET %s";
-        sql = String.format(sql, table, collectSqlSet(pairs));
+        List<String> columns = new LinkedList<>(map.keySet());
+        sql = String.format(sql, table, collectSqlSet(columns));
+        List<ValueContainer> values = columns.stream().map(map::get).collect(Collectors.toList());
+
         try {
-            return execInsert(sql);
+            return execInsert(sql, values);
         } catch (SQLException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
 
-    private String collectSqlSet(Map<String, String> pairs) {
+
+    private void setPreparedValues(PreparedStatement statement, List<ValueContainer> values) throws SQLException {
+        for (int i = 1; i <= values.size(); i++) {
+            ValueContainer container = values.get(i - 1);
+            switch (container.getType()) {
+                case LONG:
+                    statement.setLong(i, (long) container.getValue());
+                    break;
+                case INT:
+                    statement.setInt(i, (int) container.getValue());
+                    break;
+                case BOOL:
+                    statement.setBoolean(i, (boolean) container.getValue());
+                    break;
+                case FLOAT:
+                    statement.setFloat(i, (float) container.getValue());
+                    break;
+                case DOUBLE:
+                    statement.setDouble(i, (double) container.getValue());
+                    break;
+                case STRING:
+                    statement.setString(i, (String) container.getValue());
+            }
+        }
+    }
+
+    private String collectSqlSet(List<String> columns) {
         List<String> set = new LinkedList<>();
-        pairs.forEach((key, value) -> set.add(key + "=" + value));
+        columns.forEach((key) -> set.add(key + "= ?" ));
         return String.join(", ", set);
     }
 }
