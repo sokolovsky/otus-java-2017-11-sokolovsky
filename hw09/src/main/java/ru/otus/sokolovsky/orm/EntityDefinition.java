c95ccb7a996013a289e059d6facb267dc0853d97
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-01-31 17:35:53 +0300

    HW09. Added interface of pk using in table definitions.

--- hw09/src/main/java/ru/otus/sokolovsky/orm/EntityDefinition.java
+++ hw09/src/main/java/ru/otus/sokolovsky/orm/EntityDefinition.java
@@ -4,13 +4,13 @@ import ru.otus.sokolovsky.orm.annotations.Column;
 import ru.otus.sokolovsky.orm.annotations.Entity;
 
 import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class EntityDefinition<T> {
     private final String tableName;
     private Map<String, Field> columns = new HashMap<>();
+    private Set<Field> primaries = new HashSet<>();
 
     public EntityDefinition(Class<T> cls) {
         Entity entityAnnotation = cls.getAnnotation(Entity.class);
@@ -25,30 +25,51 @@ public class EntityDefinition<T> {
                     continue;
                 }
                 columns.put(field.getName(), field);
+                if (field.getAnnotation(Column.class).isPrimary()) {
+                    primaries.add(field);
+                }
             }
             iClass = iClass.getSuperclass();
         }
     }
 
-    public Map<String, String> getFieldsPairs(T model) {
-        Map<String, String> res = new HashMap<>();
+    public Map<String, ValueContainer> getFieldsValues(T model) {
+        Map<String, ValueContainer> res = new HashMap<>();
         for (Map.Entry<String, Field> entry : columns.entrySet()) {
             Field field = entry.getValue();
             field.setAccessible(true);
 
-            Object value = null;
+            ValueContainer container = null;
             try {
-                value = field.get(model);
+                Object value = field.get(model);
+                Class<?> valueClass = value.getClass();
+                if (valueClass == Integer.class) {
+                    container = new ValueContainer(ValueContainer.Type.INT, value);
+                }
+                if (valueClass == Long.class) {
+                    container = new ValueContainer(ValueContainer.Type.LONG, value);
+                }
+                if (valueClass == Boolean.class) {
+                    container = new ValueContainer(ValueContainer.Type.BOOL, value);
+                }
+                if (valueClass == Double.class) {
+                    container = new ValueContainer(ValueContainer.Type.DOUBLE, value);
+                }
+                if (valueClass == Float.class) {
+                    container = new ValueContainer(ValueContainer.Type.FLOAT, value);
+                }
+                if (valueClass == String.class) {
+                    container = new ValueContainer(ValueContainer.Type.STRING, value);
+                }
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
+                continue;
             }
-            if (value == null) {
-                value = "";
+
+            if (container == null) {
+                throw new RuntimeException(String.format("Type of column `%s` is not compatible", field.getName()));
             }
-            if (value.getClass() == String.class) {
-                value = "'" + value + "'";
-            }
-            res.put(entry.getKey(), value.toString());
+            res.put(entry.getKey(), container);
         }
         return res;
     }
@@ -90,6 +111,23 @@ public class EntityDefinition<T> {
                 e.printStackTrace();
             }
         }
+    }
+
+    public Set<String> getPrimaries() {
+        return primaries.stream().map(Field::getName).collect(Collectors.toSet());
+    }
+
+    public boolean isPrimary(String field) {
+        for (Field primary : primaries) {
+            if (primary.getName().equals(field)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isPrimary(Field field) {
+        return primaries.contains(field);
     }
 
     public String tableName() {
