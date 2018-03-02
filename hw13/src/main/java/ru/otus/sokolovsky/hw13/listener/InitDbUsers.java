3008ba956e19c4ba41426df64419087750a24cb3
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-03-02 10:00:36 +0300

    HW13. Create db users before app get started.

--- hw13/src/main/java/ru/otus/sokolovsky/hw13/listener/InitDbUsers.java
+++ hw13/src/main/java/ru/otus/sokolovsky/hw13/listener/InitDbUsers.java
@@ -1,14 +1,43 @@
 package ru.otus.sokolovsky.hw13.listener;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+import ru.otus.sokolovsky.hw13.myorm.SqlExecutor;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 @Component
 public class InitDbUsers implements ServletContextListener {
+
+    @Autowired
+    private SqlExecutor executor;
+
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
 
+        String[] sqlStrings = {
+                "INSERT INTO address SET street=\"пл Комсомольская\", id=1",
+                "INSERT INTO address SET street=\"пл Комсомольская\", id=2",
+                "INSERT INTO user SET name=\"Иван\", age=18, address_id=1, id=1",
+                "INSERT INTO user SET name=\"Петр\", age=19, address_id=2, id=2",
+                "INSERT INTO user SET name=\"Николай\", age=20, id=3",
+                "INSERT INTO phone SET number=\"1234\", user_id=1",
+                "INSERT INTO phone SET number=\"4321\", user_id=1",
+
+        };
+
+        Arrays.stream(sqlStrings).forEach(sql -> {
+            try {
+                executor.execInsert(sql);
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
