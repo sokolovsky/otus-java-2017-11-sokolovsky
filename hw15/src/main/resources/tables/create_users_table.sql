7b7b08ac3413e3a6f1c4cef61d3e86fa4241498d
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-03-21 10:31:20 +0300

    HW15. Add main resources into the project.

---
+++ hw15/src/main/resources/tables/create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS user (
+  id BIGINT(20) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255),
+  age INTEGER(3),
+  address_id BIGINT(20) DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (address_id) REFERENCES address(id)
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
