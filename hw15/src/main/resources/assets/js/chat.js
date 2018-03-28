9913d11ddb787154e0d702f4da3fadf44b2ea78c
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-03-28 15:31:43 +0300

    HW15. Refactoring of chat js code.

--- hw15/src/main/resources/assets/js/chat.js
+++ hw15/src/main/resources/assets/js/chat.js
@@ -1,18 +1,40 @@
 $(() => {
-    const data = $("#frame").data();
+    const container = $("#frame")[0];
+    const messageInput = $(container).find('.message-input input')[0];
+    const data = $(container).data();
     const login = data.login;
 
     const socket = new WebSocket("ws://localhost:10001/chat-" + login);
+    $.extend(socket, {
+        onopen: function (event) {
+            console.log('Connection is set up');
+            console.log(arguments);
+        },
+        onmessage: function () {
+            let data = $.parseJSON(event.data);
+            newMessage(data, data.login === login);
+        }
+    });
 
-    socket.onopen = function(event) {
-        console.log('Connection is set up');
-        console.log(arguments);
-    };
+    function clearInputArea() {
+        $(messageInput).val(null);
+    }
 
-    socket.onmessage = function (event) {
-        let data = $.parseJSON(event.data);
-        newMessage(data, data.login === login);
-    };
+    function scrollMessages() {
+        $(container).find(".messages").animate({ scrollTop: $(window).height() }, "fast");
+    }
+
+    function includeMessage(messageElement) {
+        $(messageElement).appendTo($(container).find('.messages ul:first'));
+    }
+
+    function setMessageAsForeign(messageElement) {
+        $(messageElement).removeClass("sent").addClass("replies")
+    }
+
+    function getInputMessageText() {
+        return $(messageInput).val();
+    }
 
     function addZeros(n, needLength) {
         needLength = needLength || 2;
@@ -22,40 +44,43 @@ $(() => {
         }
         return n
     }
+    
+    function createMessageElement(login, time, message) {
+
+        // it can to work out with template, but is a bit hardier than it need to
+        return $(
+            '<li class="sent"><img src="/assets/img/face.png" alt="" /><p><b>' + login + '</b><br/>' +
+            addZeros(time.getHours(), 2) + ':' + addZeros(time.getMinutes(), 2) + '<br/><br /><i>' +
+            message + '</i></p></li>'
+        )[0];
+    }
 
     function newMessage(data, self) {
         if($.trim(data) === '') {
             return false;
         }
         const time = new Date(data.time);
+        const messageContainer = createMessageElement(data.login, time, data.message);
 
-        const messageContainer = $(
-                '<li class="sent"><img src="/assets/img/face.png" alt="" /><p><b>' + data.login + '</b><br/>' +
-                addZeros(time.getHours(), 2)+ ':' + addZeros(time.getMinutes(), 2) + '<br/><br /><i>' +
-                data.message + '</i></p></li>'
-            )
-            .appendTo($('.messages ul'));
-
-        $('.message-input input').val(null);
-        $('.contact.active .preview').html('<span>You: </span>' + data.message);
-        $(".messages").animate({ scrollTop: $(document).height() }, "fast");
+        includeMessage(messageContainer);
+        clearInputArea();
+        scrollMessages();
 
         if (!self) {
-            messageContainer.removeClass("sent").addClass("replies");
+            setMessageAsForeign(messageContainer);
         }
     }
 
-    function getInputMessage() {
-        return $(".message-input input").val();
-    }
-
     function send() {
-        const message = getInputMessage();
+        const message = getInputMessageText();
+        if (!message) {
+            return;
+        }
         socket.send('{"message": "' + message + '"}');
         console.log("sent", '{"message": "' + message + '"}');
     }
 
-    $('.submit').click(function() {
+    $(container).find('.submit').click(function() {
         send();
     });
 
