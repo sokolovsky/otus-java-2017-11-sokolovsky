ea420d576d188715e15e888cd22db2c35f9dd326
Максим Соколовский <sokolovsky@worksolutions.ru>
2018-04-09 16:13:27 +0300

    HW16. Console. Run process use code of lecture material.

--- hw16/console/src/main/java/ru/otus/sokolovsky/hw16/console/runner/ApplicationRunner.java
+++ hw16/console/src/main/java/ru/otus/sokolovsky/hw16/console/runner/ApplicationRunner.java
@@ -1,11 +1,18 @@
 package ru.otus.sokolovsky.hw16.console.runner;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ApplicationRunner {
 
     private String command;
     private Process process;
+
+    private final StringBuffer out = new StringBuffer();
 
     public ApplicationRunner(String command) {
         this.command = command;
@@ -15,9 +22,37 @@ public class ApplicationRunner {
         ProcessBuilder pb = new ProcessBuilder(command.split(" "));
         pb.redirectErrorStream(true);
         process = pb.start();
+
+        StreamListener output = new StreamListener(process.getInputStream(), "OUTPUT");
+        output.start();
     }
 
     public void stop() {
         process.destroy();
+    }
+
+    private class StreamListener extends Thread {
+        private final Logger logger = Logger.getLogger(StreamListener.class.getName());
+
+        private final InputStream is;
+        private final String type;
+
+        private StreamListener(InputStream is, String type) {
+            this.is = is;
+            this.type = type;
+        }
+
+        @Override
+        public void run() {
+            try (InputStreamReader isr = new InputStreamReader(is)) {
+                BufferedReader br = new BufferedReader(isr);
+                String line;
+                while ((line = br.readLine()) != null) {
+                    out.append(type).append('>').append(line).append('\n');
+                }
+            } catch (IOException e) {
+                logger.log(Level.SEVERE, e.getMessage());
+            }
+        }
     }
 }
